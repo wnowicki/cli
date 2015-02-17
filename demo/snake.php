739983eeaf6f40f1627b13504586524dee31b300
wnowicki <wonowicki@gmail.com>
2015-02-17 22:17:10 +0000

    Demo snake updated

--- demo/snake.php
+++ demo/snake.php
@@ -4,8 +4,8 @@ require_once('../vendor/autoload.php');
 
 $screen = \WNowicki\Cli\Screen::make();
 
-$x = rand(0, $screen->cols());
-$y = rand(0, $screen->rows());
+$x = rand(0, $screen->getWidth()-1);
+$y = rand(0, $screen->getHeight()-1);
 
 $moves = [-1,1];
 
@@ -31,16 +31,16 @@ while (1) {
 
     $screen->reset();
 
-    $screen->putIn('*', $snake[0][0], $snake[0][1]);
-    $screen->putIn('*', $snake[1][0], $snake[1][1]);
-    $screen->putIn('*', $snake[2][0], $snake[2][1]);
-    $screen->putIn('*', $snake[3][0], $snake[3][1]);
-    $screen->putIn('*', $snake[4][0], $snake[4][1]);
-    $screen->putIn('*', $snake[5][0], $snake[5][1]);
-    $screen->putIn('*', $snake[6][0], $snake[6][1]);
-    $screen->putIn('*', $snake[7][0], $snake[7][1]);
-    $screen->putIn('*', $snake[8][0], $snake[8][1]);
-    $screen->putIn('#', $snake[9][0], $snake[9][1]);
+    $screen->put('*', $snake[0][1], $snake[0][0]);
+    $screen->put('*', $snake[1][1], $snake[1][0]);
+    $screen->put('*', $snake[2][1], $snake[2][0]);
+    $screen->put('*', $snake[3][1], $snake[3][0]);
+    $screen->put('*', $snake[4][1], $snake[4][0]);
+    $screen->put('*', $snake[5][1], $snake[5][0]);
+    $screen->put('*', $snake[6][1], $snake[6][0]);
+    $screen->put('*', $snake[7][1], $snake[7][0]);
+    $screen->put('*', $snake[8][1], $snake[8][0]);
+    $screen->put('#', $snake[9][1], $snake[9][0]);
 
     if (rand(0,10) > 7) {
         if ($a) {
@@ -54,13 +54,13 @@ while (1) {
         }
     }
 
-    $x = min($x + $dx, $screen->cols()-1);
-    $y = min($y + $dy, $screen->rows()-1);
+    $x = min($x + $dx, $screen->getWidth()-1);
+    $y = min($y + $dy, $screen->getHeight()-1);
 
     array_shift($snake);
     $snake[9] = [$y, $x];
 
-    $screen->putIn('Updated: ' . date('H:i:s d/m/Y'), -2, -28);
+    $screen->put('Updated: ' . date('H:i:s d/m/Y'), -28, -2);
 
     $screen->render();
 
